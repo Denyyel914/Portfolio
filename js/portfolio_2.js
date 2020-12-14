a47a0b011a5b03a77b4227de41dcb92ae279ad8d
Deniel Salvacion <denielsalvacion@gmail.com>
2020-12-14 09:34:14 +0800

    Added burger menu

--- js/portfolio_2.js
+++ js/portfolio_2.js
@@ -6,3 +6,24 @@ window.onscroll = () => {
         navbar.classList.remove('header-color');
     }
 };
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-list');
+    const navList = document.querySelectorAll('.nav-list li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navList.forEach((links, index) => {
+            if(links.style.animation) {
+                links.style.animation = '';
+            } else {
+                links.style.animation = `navListFade 0.5s ease forwards ${index / 7 + 2}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
